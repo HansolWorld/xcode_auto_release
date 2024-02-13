7733682f4e2f1d8d7fe2a9c975241e3114b47af8
HansolWorld <hs9269@gmail.com>
2024-02-13 12:04:57 +0900

    init

---
+++ AutoRelease/AutoRelease/AutoReleaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoReleaseApp.swift
+//  AutoRelease
+//
+//  Created by Hansol Eom on 2/13/24.
+//
+
+import SwiftUI
+
+@main
+struct AutoReleaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
